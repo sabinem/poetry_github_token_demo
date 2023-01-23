a116aeb6c4444c29bef6a77541ef7e659c741e61
Sabine Maennel <sabine.maennel@gmail.com>
2023-01-23 15:01:53 +0100

    update tests: make api test pass and add secrets test

--- tests/test_github.py
+++ tests/test_github.py
@@ -1,5 +1,10 @@
+import os
 from poetry_github_token_demo import github
 
 
 def test_call_github_api():
     github.call_github_api(with_auth=True, call_limit=100)
+
+
+def test_secret():
+    os.environ.get('MY_TEST') == 'test'
